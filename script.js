e6df3e6c8fc31d210ee081ed34f4b86c9ff648dd
Taylor Ericson <116030067+tkericson@users.noreply.github.com>
2023-04-29 23:19:13 -0500

    Add status button to book cards with toggle functionality

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ form.addEventListener("submit", (e) => {
     form.pages.value,
     form.status.checked
   );
+
   myLibrary.push(book);
   closeForm();
   displayLibrary();
@@ -31,23 +32,46 @@ function Book(title, author, pages, status) {
   this.status = status;
 }
 
+Book.prototype.changeStatus = function() {
+  this.status = !this.status;
+}
+
+function changeStatus(index) {
+  myLibrary[index].changeStatus();
+  displayLibrary();
+}
+
 function displayLibrary() {
   container.innerHTML = "";
+
   for (let i = 0; i < myLibrary.length; i++) {
     let book = myLibrary[i];
     let card = document.createElement("div");
     card.classList.add("card");
     card.innerHTML = `<h2>${book.title}</h2>
       <p>by ${book.author}</p>
-      <p>${book.pages} pages</p>
-      <p>${book.status ? "Read" : "Not Read Yet"}</p>`;
+      <p>${book.pages} pages</p>`;
+
+    let toggleStatus = document.createElement("button");
+    toggleStatus.textContent = `${book.status ? "Finished" : "In Progress"}`;
+    toggleStatus.classList.add("toggle-status");
+    toggleStatus.addEventListener('click', () => {
+      changeStatus(i);
+    });
+    card.appendChild(toggleStatus);
+
+    if(book.status) {
+      console.log("reached");
+      toggleStatus.style.backgroundColor = "forestgreen";
+    }
+
     let removeBtn = document.createElement("button");
     removeBtn.textContent = "X";
     removeBtn.classList.add("remove-btn");
-    removeBtn.addEventListener("click", function () {
-      console.log(book);
+    removeBtn.addEventListener("click", () => {
       removeBook(i);
     });
+
     card.appendChild(removeBtn);
     container.appendChild(card);
   }
